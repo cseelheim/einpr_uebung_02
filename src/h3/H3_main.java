3d8bcdd69f6d05cbe6050f86f7266bbf8f364862
Christain Seelheim <christian.seelheim@gmail.com>
2026-01-13 12:46:13 +0100

    fixed h3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -3,16 +3,17 @@ package h3;
 public class H3_main {
 
 	public static void main(String[] args) {
-		int i, j, k;
-		i = 34;
-		j = 12;
-		k = -10;
+		int i = 300;
+		int j = 400;
+		int k = 0;
 		
 		boolean ausdruck1, ausdruck2, ausdruck3;
 		ausdruck1 = i > j;
 		ausdruck2 = i > 200;
 		ausdruck3 = j > 100;
 
+		// Wir stellen sicher, dass der default für k auf -10 gesetzt wird, egal mit welchem Wert k initialisetr wird.
+		k = -10;
 
 
 		if (ausdruck1 && !ausdruck2 && !ausdruck3) {
